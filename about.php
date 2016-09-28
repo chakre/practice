b8f0df6511e1fded14b2dfb24b2e3ed076857406
Sunil Chakradhar <logodesigny@gmail.com>
2016-09-28 06:54:21 +0545

    first commit

---
+++ about.php
@@ -0,0 +1,163 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="content-type" content="text/html; charset=utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="Description" content="">
+<meta name="Keywords" content="">
+<title> Practice </title>
+
+<!-- Bootstrap -->
+<link href="css/bootstrap.css" rel="stylesheet">
+<link href="css/style.css" rel="stylesheet">
+<link href="css/responsive.css" rel="stylesheet">
+<link href="css/font-awesome.css" rel="stylesheet" media="all">
+
+<!-- Flexslider -->
+<link href="css/flexslider.css" rel="stylesheet" media="all">
+
+<!-- Google Webfonts -->
+<link href='http://fonts.googleapis.com/css?family=Raleway:400,700' rel='stylesheet' type='text/css'>
+
+<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
+
+</head>
+     <body>
+          <header class="header">
+
+          	 <div class="container">
+              <div class="col-md-3 col-sm-3">
+          	 	 <div class="biz-logo">
+                    <a href="index.php"> <img src="images/logo.png" alt="logo" ></a>
+          	 	 </div>
+              </div>
+              <div class="col-md-9 col-sm-9">
+          	 	 <nav class="navbar navbar-default" role="navigation">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">navigation</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        
+       <li><a href="index.php">HOME</a></li>
+        <li><a href="about.php">ABOUT US</a></li>
+        <li><a href="#">SERVICES</a></li>
+        <li><a href="#">FAQS</a></li>
+        <li><a href="contact.php">CONTACT US</a></li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+              </div>
+          	 </div>
+</header> 
+
+
+  <div class="content">
+    <div class="container">
+            <h2>About</h2>
+          <div class="col-md-6">
+      
+      <div class="content-part">
+        
+         <p>
+            There are many variat  ions of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, 
+            by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, 
+            you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet 
+            tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200
+             Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem 
+             Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.</p>
+             </p>
+             
+
+        
+      </div>
+    </div>
+      <div class="image">
+        <div class="col-md-6">
+             <img src="images/ashishma_nakarmi.jpg" alt="about-image">
+         </div>
+      </div>
+    </div>
+  </div>
+
+    <!--footer -->
+  <div class="blocks">
+     <div class="container">
+      <div class="row">
+         <div class="col-md-4">
+          <div class="contact-us">
+          <h2>Contact Us</h2>
+          <p>
+              Chamuna Road, 3 Nepal.</br>
+              Suneelcoool@hotmail.com</br>
+              9841663366</br>
+              www.webia.com</br>
+
+          </p>
+         </div>
+      </div>
+      <div class="col-md-4">
+         <div class="services">
+              <h2>Services</h2>
+                 <p> It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, 
+                  to generate Lorem Ipsum which looks reasonable. 
+                  The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words.</p>
+          </div>
+      </div>
+
+    <div class="col-md-4">
+       <div class="newsletter">
+         <h2>Newsletter</h2>
+        <form role="form">
+                <div class="form-group">
+                  
+                  <input type="text" class="form-control" id="newsletter" placeholder="Newsletter">
+                </div>
+               
+                <button type="submit" class="btn btn-default">Send</button>
+            </form>
+         </div>
+     </div>  
+         
+       </div>
+     </div>
+  </div>
+
+  <footer>
+    <div class="footer-content">
+       <p>
+          copy right 2014 , powered by <a href="#">sunil chakradhar</a><br>
+           www.webdeveloper.com
+       </p>
+    </div>
+  </footer>
+
+   
+
+
+
+         <!-- jQuery -->
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="js/jquery.js">\x3C/script>')</script>
+
+<!-- Bootstrap -->
+<script src="js/bootstrap.min.js" type="text/javascript"></script>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,253 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="content-type" content="text/html; charset=utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="Description" content="">
+<meta name="Keywords" content="">
+<title> Practice </title>
+
+<!-- Bootstrap -->
+<link href="css/bootstrap.css" rel="stylesheet">
+<link href="css/style.css" rel="stylesheet">
+<link href="css/responsive.css" rel="stylesheet">
+<link href="css/font-awesome.css" rel="stylesheet" media="all">
+<link rel="stylesheet" href="css/flexslider.css" type="text/css" media="screen" />
+
+<!-- Flexslider -->
+<link href="css/flexslider.css" rel="stylesheet" media="all">
+
+<!-- Google Webfonts -->
+<link href='http://fonts.googleapis.com/css?family=Raleway:400,700' rel='stylesheet' type='text/css'>
+
+<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
+
+</head>
+     <body>
+          <header class="header">
+
+          	 <div class="container">
+              <div class="col-md-3 col-sm-3">
+          	 	 <div class="biz-logo">
+                    <a href="index.php"> <img src="images/logo.png" alt="logo" ></a>
+          	 	 </div>
+              </div>
+              <div class="col-md-9 col-sm-9">
+          	 	 <nav class="navbar navbar-default" role="navigation">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">navigation</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        
+        <li><a href="index.php">HOME</a></li>
+        <li><a href="about.php">ABOUT US</a></li>
+        <li><a href="#">SERVICES</a></li>
+        <li><a href="#">FAQS</a></li>
+        <li><a href="contact.php">CONTACT US</a></li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+              </div>
+          	 </div>
+            </header> 
+
+    <div class="banner">
+
+        <section class="slider">
+        <div class="flexslider">
+          <ul class="slides">
+            <li>
+              <img src="images/scene.jpg" />
+
+            </li>
+            <li>
+              <img src="images/scene.jpg" />
+              
+            </li>
+            <li>
+              <img src="images/scene.jpg" />
+              
+            </li>
+          </ul>
+        </div>
+      </section>
+
+
+
+
+
+        
+
+        <!-- <img src="images/scene.jpg" alt="scenary">
+        
+          <a href="#" class="left"></a>
+        <a href="#" class="right"></a>
+        
+         <div class="small-circle">
+             <ul class="circle">
+              
+              <li><a href="#" class="active"></a></li>
+              <li><a href="#"></a></li>
+              <li><a href="#"></a></li>
+              <li><a href="#"></a></li>
+              <li><a href="#"></a></li>
+            </ul>
+
+          </div>  -->
+
+    </div>	
+
+    <div class="about">
+       <div class="container">
+       
+          <h2>ABOUT US</h2>
+       <p>
+            There are many variat  ions of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, 
+            by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, 
+            you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet 
+            tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200
+             Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem 
+             Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.</p>
+             <p>
+
+             All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator 
+             on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem
+              Ipsum which looks reasonable. 
+           </p>
+         
+    
+    </div>
+  </div>
+
+  <div class="history">
+    <div class="container">
+
+      <div class="col-md-7">
+       <h2>history</h2>
+        <p>
+          There are many variat  ions of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, 
+          by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum,
+           you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet 
+           tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 
+           Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem 
+           Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.
+
+             It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, 
+             to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, 
+             injected humour, or non-characteristic words.
+        </p>
+       </div>
+
+       <div class="col-md-5">
+        <div class="mobile-image">
+          <a href="#"><img src="images/mobile.png"></a>
+        </div>
+       </div>
+
+    </div>
+  </div>
+
+  <div class="blocks">
+     <div class="container">
+      <div class="row">
+         <div class="col-md-4">
+          <div class="contact-us">
+          <h2>Contact Us</h2>
+          <p>
+              Chamuna Road, 3 Nepal.</br>
+              Suneelcoool@hotmail.com</br>
+              9841663366</br>
+              www.webia.com</br>
+
+          </p>
+         </div>
+      </div>
+      <div class="col-md-4">
+         <div class="services">
+              <h2>Services</h2>
+                 <p> It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, 
+                  to generate Lorem Ipsum which looks reasonable. 
+                  The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words.</p>
+          </div>
+      </div>
+
+    <div class="col-md-4">
+       <div class="newsletter">
+         <h2>Newsletter</h2>
+        <form role="form">
+                <div class="form-group">
+                  
+                  <input type="text" class="form-control" id="newsletter" placeholder="Newsletter">
+                </div>
+               
+                <button type="submit" class="btn btn-default">Send</button>
+            </form>
+         </div>
+     </div>  
+         
+       </div>
+     </div>
+  </div>
+
+  <footer>
+    <div class="footer-content">
+       <p>
+          copy right 2014 , powered by <a href="#">sunil chakradhar</a><br>
+           www.webdeveloper.com
+       </p>
+    </div>
+  </footer>
+
+   
+
+
+
+         <!-- jQuery -->
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="js/jquery.js">\x3C/script>')</script>
+
+<!-- Bootstrap -->
+<script src="js/bootstrap.min.js" type="text/javascript"></script>
+
+<!-- flex slider -->
+
+   <!-- jQuery -->
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+  <script>window.jQuery || document.write('<script src="js/libs/jquery-1.7.min.js">\x3C/script>')</script>
+
+  <!-- FlexSlider -->
+  <script defer src="js/jquery.flexslider.js"></script>
+
+  <script type="text/javascript">
+    $(function(){
+      SyntaxHighlighter.all();
+    });
+    $(window).load(function(){
+      $('.flexslider').flexslider({
+        animation: "slide",
+        start: function(slider){
+          $('body').removeClass('loading');
+        }
+      });
+    });
+  </script>
+    </body>
+</html>
